b8ae60c532e09071d862d33aad6e5ba6c7f46355
유국현 <rnrgus5897@gmail.com>
2022-04-28 11:24:00 +0900

    first new

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express()
+const home = require('./router/router');
+const connectDB = require('./config/db');
+const path = require('path')
+
+connectDB();
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(express.static(path.join(__dirname , 'public')))
+
+app.set('view engine' , 'ejs');
+app.set('views' , './views');
+
+
+app.use('/' , home)
+const port = process.env.PORT || 5000;
+
+app.listen(port , () => {
+    console.log(`${port}포트 포트로 이동중.....`)
+})
